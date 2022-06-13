ed133aea9447cc3ef3188c2aec30ee04da717bcb
Breno Santana <44840815+LinuxLoverCoder@users.noreply.github.com>
2022-06-13 04:26:10 -0300

    Refactoring of index.js

--- asset/index.js
+++ asset/index.js
@@ -1,18 +1,20 @@
 // noinspection HtmlUnknownTarget,BadExpressionStatementJS
+let readdir1, path;
+({readdir: readdir1} = require('fs').promises);
+path = require('path');
 
-const fs = require('fs').promises;
-const path = require('path');
-
-async function readdir(rootDir) {
+const readdir = async rootDir => {
     rootDir = rootDir || path.resolve(__dirname);
-    const files = await fs.readdir(rootDir);
+    const files = await readdir1(rootDir);
     walk(files, rootDir);
-}
+};
 
-async function walk(files, rootDir) {
+const walk = async (files, rootDir) => {
     for (let file of files) {
         console.log(file);
     }
-}
+};
 const link = readdir("/");
+
+
 
